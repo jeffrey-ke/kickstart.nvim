7850d925048df89bff8fe8b79f4a32c3553ca1dc
Jeffrey Ke <jeffke613@gmail.com>
2025-09-28 11:59:30 -0400

    Using leader d and leader u instead of c-u and c-d back to normal setup

--- lua/custom/plugins/neoscroll.lua
+++ lua/custom/plugins/neoscroll.lua
@@ -1,4 +1,18 @@
 return {
   'karb94/neoscroll.nvim',
-  opts = {},
+  event = 'VeryLazy',
+  config = function()
+    local neoscroll = require 'neoscroll'
+
+    neoscroll.setup {}
+
+    -- Set up keymaps
+    local keymap = vim.keymap.set
+    keymap('n', '<leader>u', function()
+      neoscroll.ctrl_u { duration = 250 }
+    end, { desc = 'Scroll up' })
+    keymap('n', '<leader>d', function()
+      neoscroll.ctrl_d { duration = 250 }
+    end, { desc = 'Scroll down' })
+  end,
 }
