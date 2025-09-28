f23c87a5fa11d88908b3b7fccc1e13e20f90f033
Jeffrey Ke <jeffke613@gmail.com>
2025-09-28 11:57:56 -0400

    Now, blink also uses leader j and k and not c-n and c-p blink and telescope are two separate plugins!

--- init.lua
+++ init.lua
@@ -923,9 +923,10 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'enter',
-        -- ['<C-j>'] = { 'select_next', 'fallback' },
-        -- ['<C-k>'] = { 'select_prev', 'fallback' },
 
+        -- Override preset mappings
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
