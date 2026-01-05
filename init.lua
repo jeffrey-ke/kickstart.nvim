c83583e560464cecf28e57ea740d238ea903682f
jeff <jke3@cs.cmu.edu>
2026-01-04 19:37:02 -0500

    persistent nvim sessions

--- init.lua
+++ init.lua
@@ -389,6 +389,13 @@ require('lazy').setup({
       )
     end,
   },
+
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre',
+    opts = {},
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
