2bac2a9f053f8d73c12bd977b17939d0ba51c972
jeff <jke3@cs.cmu.edu>
2026-03-04 11:07:30 -0500

    vsb for :b but in a vsplit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,8 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.cmd [[cnoreabbrev vsb vert sb]]
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
