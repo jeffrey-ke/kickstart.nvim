56aa0dd242e24a746b4422ea8feff9a23ea5ba06
Jeffrey Ke <jeffke613@gmail.com>
2026-05-29 11:34:34 -0400

    image and markdown plugins

---
+++ lua/custom/plugins/image.lua
@@ -0,0 +1,16 @@
+return {
+  '3rd/image.nvim',
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  opts = {
+    processor = 'magick_cli',
+    tmux_show_only_in_active_window = true,
+  },
+  config = function(_, opts)
+    require('image').setup(opts)
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      callback = function()
+        require('image').clear()
+      end,
+    })
+  end,
+}
