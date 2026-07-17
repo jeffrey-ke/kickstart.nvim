977fe0d6adf0104b874e0b9760b3413ea29e006f
Jeffrey Ke <jeffke613@gmail.com>
2026-07-17 12:56:46 -0400

    Add latex.plugins import and performance.rtp.paths for the myvimtex overlay

--- init.lua
+++ init.lua
@@ -1318,12 +1318,14 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
+  vim.g.myvimtex_rtp_paths ~= nil and { import = 'latex.plugins' } or nil,
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
+  performance = { rtp = { paths = vim.g.myvimtex_rtp_paths or {} } },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
